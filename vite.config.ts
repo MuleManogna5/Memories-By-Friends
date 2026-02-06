5f9d37353e8a169ef6f22aa97213d5ea12e4f436
Manogna Mule <166730256+MuleManogna5@users.noreply.github.com>
2026-02-06 18:28:58 +0530

    deploy with hash router

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  // ✅ ONLY for production (GitHub Pages)
-  base: mode === "production" ? "/Memories-By-Friends/" : "/",
 
   plugins: [react()],
   resolve: {
